e1217d50c001e6bb8af30f217b74ee5ff446f4be
河澄菜々夏 <Nanaka@gakiyoshinananatsu-no-MacBook-Air.local>
2019-10-15 13:41:02 +0900

    shinjuku app, first commit

--- blog/forms.py
+++ blog/forms.py
@@ -1,9 +1,69 @@
 from django import forms
+from .models import Post, Name, Group, Route
 
-from .models import Post
+NumberOfPeople = ((1,1),(2,2),(3,3),(4,4),(5,5),)
+
+DESTINATION = (('あり','あり'),('なし','なし'),)
+
+Landmark = (('都庁','都庁'),)
+
+Exit = (('出口1','出口１'),)
 
 class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
         fields = ('title', 'text',)
+
+class NameForm(forms.ModelForm):
+
+    class Meta:
+        model = Name
+        fields = ('number', 'name',)
+
+class GroupForm(forms.ModelForm):
+
+    class Meta:
+        model = Group
+        fields = ('number', 'people', 'destination', 'landmark', 'exitmark',)
+
+"""
+        number = forms.CharField(
+        label='グループ番号',
+        max_length=100,
+        required=True,
+        )
+
+        people = forms.IntegerField(
+        label='人数',
+        required=True,
+        )
+
+        destination = forms.ChoiceField(
+        label='目的地',
+        widget=forms.Select,
+        choices=DESTINATION,
+        required=True,
+        )
+
+        landmark = forms.CharField(
+        label='ランドマーク',
+        max_length=50,
+        required=True,
+        )
+
+        exitmark = forms.CharField(
+        label='出口',
+        required=True,
+        )
+"""
+    #class Meta:
+        #model = Group
+        #fields = ('number', 'people', 'destination', 'landmark', 'exitmark',)
+
+
+class RouteForm(forms.ModelForm):
+
+    class Meta:
+        model = Route
+        fields = ('route', 'hour', 'minute',)
--- blog/urls.py
+++ blog/urls.py
@@ -2,9 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    path('post/<int:pk>)/',views.post_detail, name='post_detail'),
+    path('', views.index, name='index'),
+    path('select/', views.select, name='select'),
+    path('map/<int:pk>', views.map, name='map'),
+    path('add/<int:pk>', views.add_route, name='add_route'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
     path('post/new', views.post_new, name='post_new'),
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
-
 ]
