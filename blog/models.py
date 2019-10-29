abd264a8fa90d28ebef7f08103297c851b0ca283
河澄菜々夏 <Nanaka@gakiyoshinananatsu-no-MacBook-Air.local>
2019-10-29 13:25:20 +0900

    Add file

--- blog/models.py
+++ blog/models.py
@@ -18,19 +18,35 @@ class Cafe(models.Model):
    landmark (char) : ランドマーク(目的地)情報
    exitmark (char) : 出口(目的地)情報
 """
+def FileRead():
+    #ファイルを読み込む
+    file_data = open("exit.txt", "r")
+    firstline = True
+    #読み込んだファイルを1行ずつ表示
+    exit = []
+    for line in file_data:
+        data = line.split(' ')#空白文字で区切る
+        userval = str(data[0])#データベースに入れる値
+        dbval = int(data[1])#ユーザーが見る値
+        exit.append([dbval, userval])#出口
+    #開いたファイルを閉じる
+    file_data.close()
+    return(exit)
 
 NumberOfPeople = ((2,2),(3,3),(4,4),(5,5),)
-DESTINATION = (('あり','あり'),('なし','なし'),)
-Landmark = ((100,'都庁'),(200,'新宿ピカデリー'),)
-Exit = ((300,'出口１'),)
-Route = ((1,'小田急小田原線'),(2,'都営新宿線'),(3,'東京メトロ丸ノ内線'),(4,'JR中央線'),(5,'JR埼京線'),(6,'JR総務線'),(7,'JR山の手線(外回り)'),(8,'JR山の手線(内回り)'),(9,'JR湘南新宿ライン'),(10,'京王線'),(11,'京王新線'),(12,'西武新宿線'),)
+DESTINATION = ((True,'あり'),(False,'なし'),)
+Landmark = ((-1,'-------'),(100,'都庁'),(200,'新宿ピカデリー'),)
+Route = ((1,'小田急小田原線'),(2,'都営新宿線'),(3,'東京メトロ丸ノ内線'),(4,'JR中央線'),(5,'JR埼京線'),(6,'JR総務線'),(7,'JR山の手線(外回り)'),(8,'JR山の手線(内回り)'),(9,'JR湘南新宿ライン'),(10,'京王線'),(11,'京王新線'),(0,'西武新宿線'),)
+
+#Exit = FileRead()
+Exit = ((1,'出口１'),)
 
 class Group(models.Model):
     number = models.CharField(max_length=100)
     people = models.IntegerField(choices=NumberOfPeople)
-    destination = models.CharField(max_length=10,choices=DESTINATION)
-    landmark = models.CharField(max_length=20,choices=Landmark,blank=True)
-    exitmark = models.CharField(max_length=20,choices=Exit,blank=True)
+    destination = models.BooleanField(max_length=10,choices=DESTINATION)
+    landmark = models.IntegerField(choices=Landmark,default=0)
+    exitmark = models.IntegerField(choices=Exit,default=0)
 
     def __str__(self):
         return self.number
@@ -44,7 +60,7 @@ class Group(models.Model):
 """
 class Route(models.Model):
     number = models.CharField(max_length=100)
-    route = models.CharField(max_length=20,choices=Route)
+    route = models.IntegerField(choices=Route)
     hour = models.CharField(max_length=10)
     minute = models.CharField(max_length=10)
 
