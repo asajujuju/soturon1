72905fc044bfebc5d0e8626e8d936e0484e5478f
Nanako-N <g16929nn@gm.tsuda.ac.jp>
2019-11-15 00:58:51 +0900

    11/15現在の最新版

--- blog/admin.py
+++ blog/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post, Cafe, Name, Group, Route
+from .models import Cafe, Group, Route
 
-admin.site.register(Post)
 admin.site.register(Cafe)
-admin.site.register(Name)
 admin.site.register(Group)
 admin.site.register(Route)
