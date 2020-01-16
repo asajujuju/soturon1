1d588930d884563fccdf069a7832895a2bb9c69f
asajujuju <38307793+asajujuju@users.noreply.github.com>
2020-01-16 19:15:45 +0900

    Update views.py

--- blog/views.py
+++ blog/views.py
@@ -40,8 +40,8 @@ def select(request):
 
 def FileRead(t):
     #ファイルを読み込む
-    #file_data = open("/home/nanako/nanako.pythonanywhere.com/" + t, "r")
-    file_data = open(t, "r")
+    file_data = open("/home/ri2/ri2.pythonanywhere.com/" + t, "r")
+    #file_data = open(t, "r")
     firstline = True
     #読み込んだファイルを1行ずつ表示
     exit = []
@@ -61,8 +61,8 @@ startrout.txtを読み込む
 """
 def GateFileRead():
     #ファイルを読み込む
-    #file_data = open("/home/nanako/nanako.pythonanywhere.com/startroute.txt", "r")
-    file_data = open("startroute.txt", "r")
+    file_data = open("/home/ri2/ri2.pythonanywhere.com/startroute.txt", "r")
+    #file_data = open("startroute.txt", "r")
     firstline = True
     #読み込んだファイルを1行ずつ表示
     StationSize = []
@@ -82,8 +82,8 @@ def GateFileRead():
 """
 def point(meet_node):
     #ファイルを読み込む
-    #file_data = open("/home/nanako/nanako.pythonanywhere.com/point.txt", "r")
-    file_data = open("point.txt", "r")
+    file_data = open("/home/ri2/ri2.pythonanywhere.com/point.txt", "r")
+    #file_data = open("point.txt", "r")
     firstline = True
     #読み込んだファイルを1行ずつ表示
     MeetToPoint = []
